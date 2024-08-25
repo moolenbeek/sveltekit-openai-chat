66804042974c6c5585eeb6dc872f2af28af08636
moolenbeek <emoolenbeek@gmail.com>
2024-08-25 13:56:13 -0400

    make api call with prompt and display result

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,28 @@
 import type { Actions } from "./$types";
+import { env } from "$env/dynamic/private";
+
+import { OpenAI } from "openai";
+const openai = new OpenAI({
+	apiKey: env.OPENAI_API_KEY
+});
+export default openai;
 
 export const actions = {
 	default: async ({ request }) => {
-    const data = await request.formData();
-    const message = data.get('message');
+		const form = await request.formData();
+		const prompt = form.get("prompt");
 
-    console.log(`Message: ${message}`);
+		const completion = await openai.chat.completions.create({
+			model: "gpt-4o-mini",
+			messages: [
+				{
+					role: "user",
+					content: `${prompt}`
+				}
+			]
+		});
+
+		const data = completion.choices[0].message;
+		return data;
 	}
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,36 @@
+<script lang="ts">
+	let prompt = "";
+	let messages: string[] = [];
+
+	async function sendPrompt() {
+		const response = await fetch("/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: `prompt=${encodeURIComponent(prompt)}`
+		});
+
+		const message = await response.json();
+		messages = [...messages, message.data];
+	}
+</script>
+
 <h1>Open AI</h1>
 
-<form method="POST">
-	<input name="message" type="text" /> <button>submit</button>
+<form method="POST" on:submit|preventDefault={sendPrompt}>
+	<label for="prompt">
+		<textarea name="prompt" rows="4" bind:value={prompt} />
+	</label>
+	<button type="submit">submit</button>
 </form>
+
+{#each messages as message}
+	<pre>{JSON.parse(message)[2]}</pre>
+{/each}
+
+<style>
+	h1 {
+		font-family: Archivo, sans-serif;
+	}
+</style>
