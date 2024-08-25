0ecca43a0f1ebf27a18dfb8d1143f07b4813b6bc
moolenbeek <emoolenbeek@gmail.com>
2024-08-25 15:58:18 -0400

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,21 +16,35 @@
 	}
 </script>
 
-<h1>Open AI</h1>
+<div class="container">
+	<h1 id="title">Open AI</h1>
 
-<form method="POST" on:submit|preventDefault={sendPrompt}>
-	<label for="prompt">
-		<textarea name="prompt" rows="4" bind:value={prompt} />
-	</label>
-	<button type="submit">submit</button>
-</form>
+	<form method="POST" on:submit|preventDefault={sendPrompt}>
+		<label for="prompt">
+			<textarea name="prompt" rows="4" bind:value={prompt} />
+		</label>
+		<button type="submit" id="submitButton">submit</button>
+	</form>
+</div>
 
 {#each messages as message}
 	<pre>{JSON.parse(message)[2]}</pre>
 {/each}
 
 <style>
-	h1 {
+	form {
+		display: inline-block;
+	}
+	.container {
+		padding: 1em;
+		text-align: center;
+	}
+	#title {
 		font-family: Archivo, sans-serif;
+		flex: 1;
+		text-align: center;
+	}
+	#submitButton {
+		display: block;
 	}
 </style>
